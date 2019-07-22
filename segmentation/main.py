7e0965a8fb08578f12a2eaf8fafc1ef397e66353
Guilherme Esgario <guilherme.esgario@hotmail.com>
2019-07-22 08:10:39 -0300

    Update segmentation

--- segmentation/main.py
+++ segmentation/main.py
@@ -1,131 +1,33 @@
-import os
+import sys
+import warnings
+import argparse
+from segmentation import SemanticSegmentation
 
-import torch
-from torch import nn
-from torch import optim
-from torch.optim.lr_scheduler import MultiStepLR
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
 
-from tqdm import tqdm
-import click
-import numpy as np
-
-from pspnet import PSPNet
-
-import torchvision.transforms as transforms
-from utils.customdatasets import CoffeeSegmentationLoader
-import utils.augmentations as aug
-
-
-def data_loader(split='train', batch_size=4):
-    # Augmentations
-    if split == 'train':
-        augs = aug.Compose([aug.RandomRotate(10), aug.RandomHorizontallyFlip(0.5)])
-        shuffle = True
-    else:
-        augs = None
-        shuffle = False
-
-    dataset = CoffeeSegmentationLoader(root='dataset/', augmentations=augs, split=split, img_size=(224, 224))
-    loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle)
-    
-    class_weight = torch.tensor([1., 1., 1.])
-    if torch.cuda.is_available():
-        class_weight = class_weight.cuda()
-
-    return loader, class_weight, len(dataset)
-
-## Declare models
-models = {
-    'squeezenet': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=512, deep_features_size=256, backend='squeezenet'),
-    'densenet': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=1024, deep_features_size=512, backend='densenet'),
-    'resnet18': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=512, deep_features_size=256, backend='resnet18'),
-    'resnet34': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=512, deep_features_size=256, backend='resnet34'),
-    'resnet50': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=2048, deep_features_size=1024, backend='resnet50'),
-    'resnet101': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=2048, deep_features_size=1024, backend='resnet101'),
-    'resnet152': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=2048, deep_features_size=1024, backend='resnet152')
-}
-
-# Building network
-def build_network(snapshot, backend):
-    epoch = 0
-    backend = backend.lower()
-    
-    # Instantiate network model
-    net = models[backend]()
-    
-    # Parallel training
-    if torch.cuda.device_count() > 1:
-        net = nn.DataParallel(net)
-    
-    # Load a pretrained network    
-    if snapshot is not None:
-        _, epoch = os.path.basename(snapshot).split('_')
-        epoch = int(epoch)
-        net.load_state_dict(torch.load(snapshot))
-    
-    # Sending model to GPU
-    if torch.cuda.is_available():
-        net = net.cuda()
-        
-    return net, epoch
-
-# Arguments
-@click.command()
-@click.option('--models-path', type=str, default='net_weights', help='Path for storing model snapshots')
-@click.option('--backend', type=str, default='resnet50', help='Feature extractor')
-@click.option('--snapshot', type=str, default=None, help='Path to pretrained weights')
-@click.option('--batch-size', type=int, default=4)
-@click.option('--alpha', type=float, default=1.0, help='Coefficient for classification loss term')
-@click.option('--epochs', type=int, default=20, help='Number of training epochs to run')
-@click.option('--gpu', type=str, default='0', help='List of GPUs for parallel training, e.g. 0,1,2,3')
-@click.option('--start-lr', type=float, default=0.001)
-@click.option('--milestones', type=str, default='10,20,30', help='Milestones for LR decreasing')
-def train(models_path, backend, snapshot, batch_size, alpha, epochs, start_lr, milestones, gpu):
-    os.environ["CUDA_VISIBLE_DEVICES"] = gpu
-    model, starting_epoch = build_network(snapshot, backend)
-    models_path = os.path.abspath(os.path.expanduser(models_path))
-    os.makedirs(models_path, exist_ok=True)
-    
-    '''
-        To follow this training routine you need a DataLoader that yields the tuples of the following format:
-        (Bx3xHxW FloatTensor x, BxHxW LongTensor y, BxN LongTensor y_cls) where
-        x - batch of input images,
-        y - batch of groung truth seg maps,
-        y_cls - batch of 1D tensors of dimensionality N: N total number of classes, 
-        y_cls[i, T] = 1 if class T is present in image i, 0 otherwise
-    '''
-    train_loader, class_weights, n_images = data_loader('train', batch_size)
-    
-    optimizer = optim.Adam(model.parameters(), lr=start_lr)
-    scheduler = MultiStepLR(optimizer, milestones=[int(x) for x in milestones.split(',')])
-    
-    for epoch in range(starting_epoch, starting_epoch + epochs):
-        seg_criterion = nn.NLLLoss(weight=class_weights)
-        cls_criterion = nn.BCEWithLogitsLoss(weight=class_weights)
-        epoch_losses = []
-        train_iterator = tqdm(train_loader, total=n_images // batch_size + 1)
-        model.train()
-        
-        for x, y, y_cls in train_iterator:
-            optimizer.zero_grad()
-            x, y, y_cls = x.cuda(), y.cuda(), y_cls.cuda()
-            out, out_cls = model(x)
-            seg_loss, cls_loss = seg_criterion(out, y), cls_criterion(out_cls, y_cls)
-            loss = seg_loss + alpha * cls_loss
-            epoch_losses.append(loss.data.cpu())
-            #status = '[{0}] loss = {1:0.5f} avg = {2:0.5f}, LR = {5:0.7f}'.format(
-            #    epoch + 1, loss.data.cpu(), np.mean(epoch_losses), scheduler.get_lr())
-            status = '[%i] loss = %.5f avg = %.5f, LR = %.7f' % (epoch + 1, loss.data.cpu(), np.mean(epoch_losses), scheduler.get_lr()[0])
-            train_iterator.set_description(status)
-            loss.backward()
-            optimizer.step()
-            
-        scheduler.step()
-        torch.save(model.state_dict(), os.path.join(models_path, '_'.join(["PSPNet", str(epoch + 1)])))
-        train_loss = np.mean(epoch_losses)
-
-        
 if __name__ == '__main__':
-    train()
+
+    parser = argparse.ArgumentParser()
+    
+    # Training settings
+    parser.add_argument('--optimizer', type=str, default='adam')
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--weight_decay', type=float, default=5e-4)
+    parser.add_argument('--snapshot', type=str, default=None, help='Path to pretrained weights')
+    parser.add_argument('--extractor', type=str, default='resnet18')
+    parser.add_argument('--epochs', type=int, default=100)
+    # Output filename
+    parser.add_argument('--output_filename', type=str, default='resnet50_sgd_32_mixup')
+    # Train and Validation -> True, Test -> False
+    parser.add_argument("--train", type=bool, default=True)
+    
+    options = parser.parse_args()
+    
+    # -------------------------------------------------------- #
+    Seg = SemanticSegmentation(parser)
+    
+    if options.train:
+        Seg.run_training()
+    else:
+        Seg.run_test()
---
+++ segmentation/segmentation.py
@@ -0,0 +1,334 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: esgario
+"""
+
+import os
+import sys
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+from torch import optim
+from torch.utils.data.sampler import WeightedRandomSampler
+from torch.nn.utils.clip_grad import clip_grad_norm_
+from torch.optim.lr_scheduler import MultiStepLR
+
+import utils.augmentations as aug
+from utils.customdatasets import CoffeeSegmentationLoader
+from utils.metric import scores
+from net_models import PSPNet
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix
+
+import pickle
+import math
+
+## Declare models
+models = {
+    'squeezenet': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=512, deep_features_size=256, backend='squeezenet'),
+    'densenet': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=1024, deep_features_size=512, backend='densenet'),
+    'resnet18': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=512, deep_features_size=256, backend='resnet18'),
+    'resnet34': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=512, deep_features_size=256, backend='resnet34'),
+    'resnet50': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=2048, deep_features_size=1024, backend='resnet50'),
+    'resnet101': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=2048, deep_features_size=1024, backend='resnet101'),
+    'resnet152': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=2048, deep_features_size=1024, backend='resnet152')
+}
+
+# Building network
+def build_network(snapshot, backend):
+    epoch = 0
+    backend = backend.lower()
+    
+    # Instantiate network model
+    net = models[backend]()
+    
+    # Parallel training
+    if torch.cuda.device_count() > 1:
+        net = nn.DataParallel(net)
+    
+    # Load a pretrained network    
+    if snapshot is not None:
+        _, epoch = os.path.basename(snapshot).split('_')
+        epoch = int(epoch)
+        net.load_state_dict(torch.load(snapshot))
+    
+    # Sending model to GPU
+    if torch.cuda.is_available():
+        net = net.cuda()
+        
+    return net, epoch
+
+def adjust_learning_rate(optimizer, epoch, opt):
+    if opt.optimizer == 'sgd':
+        lr_values = [ 0.01, 0.005, 0.001, 0.0005, 0.0001 ]
+        step = round(opt.epochs / 5)
+
+        idx = min(math.floor(epoch/step), len(lr_values))
+        learning_rate = lr_values[idx]
+
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = learning_rate
+
+    return optimizer
+
+def data_loader(split='train', batch_size=4):
+    # Augmentations
+    if split == 'train':
+        augs = aug.Compose([aug.RandomRotate(10), aug.RandomHorizontallyFlip(0.5)])
+        shuffle = True
+    else:
+        augs = None
+        shuffle = False
+
+    dataset = CoffeeSegmentationLoader(root='dataset/', augmentations=augs, split=split, img_size=(64, 64))
+    loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle)
+    
+    class_weight = torch.tensor([1., 1., 1.])
+    if torch.cuda.is_available():
+        class_weight = class_weight.cuda()
+
+    return loader, class_weight, len(dataset)
+
+def eval_metric(label_trues, label_preds, n_class):
+    label_preds = torch.max(label_preds, 1)[1]
+    
+    if len(label_trues.shape) > 2:
+        acc, miou = [], []
+        for i in range(label_trues.shape[0]):
+            score = scores(label_trues[i].cpu(), label_preds[i].cpu(), n_class)[0]
+            miou.append(score['mean iou'])
+            acc.append(score['overall acc'])
+        return np.mean(miou), np.mean(acc) 
+    else:
+        score = scores(label_trues.cpu(), label_preds.cpu(), n_class)[0]
+        return score['mean iou'], score['overall acc']
+    
+# -------------------------------------------------------------------------------------- #
+    
+class SemanticSegmentation:
+    def __init__(self, parser):
+        self.opt = parser.parse_args()
+        
+    def train(self, train_loader, n_images, batch_size, epoch, model, seg_criterion, cls_criterion, optimizer):
+        
+        model.train()
+        train_metrics = { 'loss': [], 'miou': [], 'acc': [] }
+        
+        train_iterator = tqdm(train_loader, total=n_images // batch_size + 1)            
+        for x, y, y_cls in train_iterator:
+            
+            # Loading images on gpu
+            if torch.cuda.is_available():
+                x, y, y_cls = x.cuda(), y.cuda(), y_cls.cuda()
+            
+            # Pass images through the network
+            out, out_cls = model(x)
+            
+            # Compute error
+            seg_loss, cls_loss = seg_criterion(out, y), cls_criterion(out_cls, y_cls)
+            loss = seg_loss + cls_loss
+            
+            # Clear gradients parameters
+            model.zero_grad()            
+            
+            # Getting gradients
+            loss.backward()
+            
+            # Updating parameters
+            optimizer.step()
+            
+            # Metrics
+            train_metrics['loss'].append(loss.data.cpu())
+            metrics = eval_metric(y, out, 3)
+            train_metrics['miou'].append(metrics[0])
+            train_metrics['acc'].append(metrics[1])
+            
+            status = '[%i] loss = %.4f avg = %.4f, miou = %.4f, acc = %.4f' % (epoch + 1,
+                                                                              loss.data.cpu(),
+                                                                              np.mean(train_metrics['loss']),
+                                                                              np.mean(train_metrics['miou']),
+                                                                              np.mean(train_metrics['acc']))
+            train_iterator.set_description(status)
+        
+        train_metrics['loss'] = np.mean(train_metrics['loss'])
+        train_metrics['miou'] = np.mean(train_metrics['miou'])
+        train_metrics['acc'] = np.mean(train_metrics['acc'])
+        return train_metrics
+    
+    def validation(self, val_loader, n_images, batch_size, epoch, model, seg_criterion, cls_criterion):
+        # tell to pytorch that we are evaluating the model
+        model.eval()
+    
+        val_metrics = { 'loss': [], 'miou': [], 'acc': [] }
+        
+        val_iterator = tqdm(val_loader, total=n_images // batch_size + 1)            
+        
+        with torch.no_grad():
+            for x, y, y_cls in val_iterator:
+                # Loading images on gpu
+                if torch.cuda.is_available():
+	                x, y, y_cls = x.cuda(), y.cuda(), y_cls.cuda()
+
+	            # pass images through the network
+                out, out_cls = model(x)
+                
+                # Computer error
+                seg_loss, cls_loss = seg_criterion(out, y), cls_criterion(out_cls, y_cls)
+                loss = seg_loss + cls_loss
+
+	            # Compute metrics
+	            ## Loss
+                # Metrics
+                val_metrics['loss'].append(loss.data.cpu())
+                metrics = eval_metric(y, out, 3)
+                val_metrics['miou'].append(metrics[0])
+                val_metrics['acc'].append(metrics[1])
+
+                status = '[%i] loss = %.4f avg = %.4f, miou = %.4f, acc = %.4f' % (epoch + 1,
+                                                                              loss.data.cpu(),
+                                                                              np.mean(val_metrics['loss']),
+                                                                              np.mean(val_metrics['miou']),
+                                                                              np.mean(val_metrics['acc']))
+                val_iterator.set_description(status)
+        
+        val_metrics['loss'] = np.mean(val_metrics['loss'])
+        val_metrics['miou'] = np.mean(val_metrics['miou'])
+        val_metrics['acc'] = np.mean(val_metrics['acc'])
+        return val_metrics
+
+    def print_info(self, **kwargs):
+        data_type = kwargs.get('data_type')
+        metrics = kwargs.get('metrics')
+        epoch = kwargs.get('epoch')
+        epochs = kwargs.get('epochs')
+
+        print('[Epoch:%3d/%3d][%s][loss: %4.2f][mIoU: %5.2f][acc: %5.2f]' %
+                (epoch+1, epochs, data_type, metrics['loss'], metrics['miou'], metrics['acc']))
+        
+    
+    def run_training(self):
+        # Dataset
+        train_loader, class_weights, n_images_train = data_loader('train', self.opt.batch_size)
+        val_loader, _, n_images_val = data_loader('val', self.opt.batch_size)
+        
+        # Model
+        model, starting_epoch = build_network(self.opt.snapshot, self.opt.extractor)
+        os.makedirs(os.path.abspath('net_weights'), exist_ok=True)
+
+        # Criterion
+        seg_criterion = nn.NLLLoss(weight=class_weights)
+        cls_criterion = nn.BCEWithLogitsLoss(weight=class_weights)
+
+        # Optimizer
+        if self.opt.optimizer == 'sgd':
+            optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=self.opt.weight_decay)
+        else:
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=self.opt.weight_decay)
+
+        record = {}
+        record['extractor'] = self.opt.extractor
+        record['batch_size'] = self.opt.batch_size
+        record['weight_decay'] = self.opt.weight_decay
+        record['optimizer'] = self.opt.optimizer
+        record['epochs'] = self.opt.epochs
+        record['train_loss'] = []
+        record['val_loss'] = []
+        record['train_miou'] = []
+        record['val_miou'] = []
+        record['train_acc'] = []
+        record['val_acc'] = []
+        
+        os.makedirs(os.path.abspath('log'), exist_ok=True)
+
+        best_loss = 1000.0
+        
+        for epoch in range(starting_epoch, starting_epoch + self.opt.epochs):
+                
+            # Training
+            train_metrics = self.train(train_loader, n_images_train, self.opt.batch_size, epoch, model, seg_criterion, cls_criterion, optimizer)
+            self.print_info(data_type='TRAIN', metrics=train_metrics, epoch=epoch, epochs=self.opt.epochs)
+
+            # Validation
+            val_metrics = self.validation(val_loader, n_images_val, self.opt.batch_size, epoch, model, seg_criterion, cls_criterion)
+            self.print_info(data_type='VAL', metrics=val_metrics, epoch=epoch, epochs=self.opt.epochs)
+
+            # Adjust learning rate
+            optimizer = adjust_learning_rate(optimizer, epoch, self.opt)
+
+            # Recording metrics
+            record['train_loss'].append(train_metrics['loss'])
+            record['train_miou'].append(train_metrics['miou'])
+            record['train_acc'].append(train_metrics['acc'])
+
+            record['val_loss'].append(val_metrics['loss'])
+            record['val_miou'].append(val_metrics['miou'])
+            record['val_acc'].append(val_metrics['acc'])
+
+            # Record best model
+            curr_loss = val_metrics['loss']
+            if (curr_loss < best_loss):
+                best_loss = curr_loss
+
+                # Saving model
+                torch.save(model, 'net_weights/PSPNet.pth')
+                
+                print('model saved')
+
+            # Saving log
+            fp = open('log/PSPNet.pkl', 'wb')
+            pickle.dump(record, fp)
+            fp.close()
+
+        # Plot
+        # static_graph(np.array(record['train_dis_acc'])/100, np.array(record['val_dis_acc'])/100)
+
+    def run_test(self):
+        os.makedirs(os.path.abspath('results'), exist_ok=True)
+        
+        # Dataset
+        test_loader, class_weights, n_images = data_loader('test', self.opt.batch_size)
+
+        # Loading model
+        model = torch.load('net_weights/PSPNet.pth')
+        model.cuda()
+        
+        # tell to pytorch that we are evaluating the model
+        model.eval()
+        
+        test_metrics = { 'miou': [], 'acc': [] }
+
+        with torch.no_grad():
+            for x, y, y_cls in test_loader:
+                # Loading images on gpu
+                if torch.cuda.is_available():
+                    x, y, y_cls = x.cuda(), y.cuda(), y_cls.cuda()
+        
+                # pass images through the network
+                out, out_cls = model(x)
+    
+                # Compute metrics
+                metrics = eval_metric(y, out, 3)
+                test_metrics['miou'].append(metrics[0])
+                test_metrics['acc'].append(metrics[1])            
+
+        miou = np.mean(test_metrics['miou'])
+        acc = np.mean(test_metrics['acc'])
+
+        f = open('results/' + self.opt.output_filename + '.csv', 'w')
+        f.write('miou,acc\n%.2f,%.2f\n' % (miou*100, acc*100))
+        f.close()
+
+
+    def get_n_params(self):
+        model = torch.load('net_weights/' + '/' + self.opt.output_filename + '.pth')
+        pp=0
+        for p in list(model.parameters()):
+            nn=1
+            for s in list(p.size()):
+                nn = nn*s
+            pp += nn
+        return pp
