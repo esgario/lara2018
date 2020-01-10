8d3420eed30f33cb9987e9beab7eb7b8b4d87659
Guilherme Esgario <guilherme.esgario@gmail.com>
2020-01-10 00:39:59 -0300

    Change aurgment default values

--- segmentation/main.py
+++ segmentation/main.py
@@ -15,13 +15,13 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=4)
     parser.add_argument('--weight_decay', type=float, default=5e-4)
     parser.add_argument('--snapshot', type=str, default=None, help='Path to pretrained weights')
-    parser.add_argument('--extractor', type=str, default='unetresnet50')
+    parser.add_argument('--extractor', type=str, default='pspresnet50')
     parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--data_augmentation', type=str, default='std')
     # Filename
-    parser.add_argument('--filename', type=str, default='unet_std')
+    parser.add_argument('--filename', type=str, default='default')
     # Train and Validation -> True, Test -> False
-    parser.add_argument("--train", type=bool, default=False)
+    parser.add_argument('--train', action='store_true')
     
     options = parser.parse_args()
     
